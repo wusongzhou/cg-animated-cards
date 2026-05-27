672864023642c75055783a9996f363bbad6cc413
wsz <wusongzhou19@qq.com>
2026-05-27 14:59:57 +0800

    chore: remove unused imports, dead CSS rules, and unused variables
    - Remove unused ScrollTrigger imports from 6 components (registered once in SmoothScroll)
    - Remove unused --yellow CSS variable from globals.css
    - Remove dead .ctaButton:hover rule overridden by GSAP runtime styles

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -2,7 +2,6 @@
 
 import { useRef } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import styles from "./Cta.module.css";
 
